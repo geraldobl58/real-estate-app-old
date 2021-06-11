1f138c7a88979333b066987534cd8cc182e397be
Geraldo Luiz <geraldobl58@gmail.com>
2021-06-10 21:01:52 -0300

    feat: Creating Discount Card Component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,10 +3,11 @@ import React, { useState, useEffect } from 'react';
 import Header from '../../components/Header';
 import Banner from '../../components/Banner';
 import CardFeature from '../../components/CardFeature';
+import CardDiscount from '../../components/CardDiscount';
 
 import api from '../../services/api';
 
-import { Wrapper, NoEmpty } from './styled';
+import { Wrapper } from './styled';
 
 export default function Home() {
   const [properties, setProperties] = useState([]);
@@ -24,11 +25,8 @@ export default function Home() {
       <Header />
       <Banner />
       <Wrapper>
-        {properties.length >= 1 ? (
-          <CardFeature properties={properties} />
-        ) : (
-          <NoEmpty>Não existe imóveis em detaque no momento</NoEmpty>
-        )}
+        {properties.length >= 1 && <CardFeature properties={properties} />}
+        {properties.length >= 1 && <CardDiscount properties={properties} />}
       </Wrapper>
     </>
   );
